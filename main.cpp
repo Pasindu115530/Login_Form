b93ef6aabc186bc104376a7be40215119eb68174
Pasindu115530 <pasinduudana12m2@gmail.com>
2024-09-25 23:28:48 +0530

    Add delete user and modify login user

--- main.cpp
+++ main.cpp
@@ -4,6 +4,8 @@
 
 using namespace std;
 
+char cho;
+
 class User{
     private:
         string username,password;
@@ -37,31 +39,60 @@ class UserManager{
 
             cout << " User Register Sucessfullyy....." << endl;
         }     
-        void LoginUser(){
-            string name,pass;
-            cout << "Enter your username : " ;
-            cin >> name;
-            cout << "Enter your password : " ;
-            cin >> pass;
-            for(int i = 0 ; i < users.size() ; i++){
-                if(users[i].getName() == name && users[i].getPassword() == pass){
-                    cout << "Login Sucessfully......." << endl;
-                    break;
-                }
-                else{
-                    cout << "Wrong Passwod " << endl;
-                    break;
-                }
+
+ void LoginUser() {
+    string cho; // Declare cho here to use in the loop
+    do {
+        string name, pass;
+        cout << "Enter your username: ";
+        cin >> name;
+        cout << "Enter your password: ";
+        cin >> pass;
+
+        bool loginSuccess = false; // Flag to track successful login
+
+        for (int i = 0; i < users.size(); i++) {
+            // Check for matching username and password
+            if (users[i].getName() == name && users[i].getPassword() == pass) {
+                cout << "Login Successfully......." << endl;
+                loginSuccess = true; // Set flag to true
+                break; // Exit the loop since the user is found
             }
-       }
+        }
+
+        if (!loginSuccess) { // If login was not successful
+            cout << "Wrong username or password." << endl;
+            cout << "Do you want to try again? (Y/N): ";
+            cin >> cho;
+        } else {
+            cho = "N"; // If successful, set cho to "N" to exit the loop
+        }
+
+    } while (cho != "N" && cho != "n"); // Repeat until user chooses to exit
+}
+
+
        void ShowUser(){
             for(int i = 0 ; i < users.size() ; i ++){
                 cout << i+1 << " user name is " <<  users[i].getName() << endl;
             }
        }
+       void DeleteUser(){
+            string name;
+            cout << "Enter the name do you want to delete :  " ;
+            cin >> name;
+            for(int i = 0 ; i < users.size() ; i ++){
+                if(users[i].getName() == name){
+                    users.erase(users.begin()+i);
+                    break;
 
+                }
+
+            }
+       }
 
 };
+
 
 int main(){
     UserManager usermanage;
@@ -87,9 +118,15 @@ int main(){
         case 3:
             usermanage.ShowUser();
             break;
+        case 4:
+            usermanage.DeleteUser();
+            break;
+        case 5:
+            cout << "Thank you for joining with us....." << endl;    
+
 
 
     }
- }while(op != 6);
+ }while(op != 5);
     return 0;
 }
